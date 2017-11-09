850716428bd614d5aba6236da902a892f4e2b530
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-09 06:54:18 -0600

    WIP; ​Creating​ seeds file, to have data to play with

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: 'Mallory', password: 'password',
+  password_confirmation: 'password')
+
+User.create(name: 'Joey', password: 'password',
+  password_confirmation: 'password')
+
+Hotel.create(name: 'The Hilton', amenities: 'A continental breakfast buffet is provided. Breakfast is based on 2 guests. Extra adults guests will be charged for breakfast.', photo: 'hilton_building.jpg')
+Hotel.create(name: 'Best Western', amenities: 'A continental breakfast buffet & High-speed Internet access ($13.95 for a 24-hour period)', photo: 'bestwest_buliding.jpg')
