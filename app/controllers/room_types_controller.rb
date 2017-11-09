5c00b62a43d87d5d585d0a0b5a588b34538f97cd
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-09 17:55:29 -0600

    WIP; Linked each picture to their nested route show page, i.e: 'rooms/5/room_types/5', shows the Classic King room

--- app/controllers/room_types_controller.rb
+++ app/controllers/room_types_controller.rb
@@ -1,2 +1,9 @@
 class RoomTypesController < ApplicationController
+  before_action :set_roomtype
+
+  private
+  def set_roomtype
+    # raise params.inspect
+    @room_type = RoomType.find_by(id: params[:id])
+  end
 end
