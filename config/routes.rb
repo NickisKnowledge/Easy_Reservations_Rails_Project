2e7bea47ffc7a1629a34eeba15ce5aa119a66f87
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-09 06:42:28 -0600

    Used Rails resource generator to make the models, controllers, views and starting routes for my app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :addresses
+  resources :reservations
+  resources :rooms
+  resources :room_types
+  resources :hotels
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
