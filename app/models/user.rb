ee1ad90dbb012ab171e88037ba2ad84c5c890893
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-09 16:41:59 -0600

    WIP; Forgot 'has_secure_password' validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_many :reservations, dependent: :destroy
   has_many :rooms, through: :reservations
   has_many :addresses, dependent: :destroy
+
+  has_secure_password
 end
