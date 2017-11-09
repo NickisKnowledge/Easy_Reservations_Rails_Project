815e57bc370ae4209f20081b38c783e555c6fa84
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-09 06:52:19 -0600

    Added ActiveRecord associations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ActiveRecord::Base
   belongs_to :room_type
   belongs_to :hotel
+  has_many :reservations
+  has_many :users, through: :reservations
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :reservations, dependent: :destroy
+  has_many :rooms, through: :reservations
+  has_many :addresses, dependent: :destroy
 end
