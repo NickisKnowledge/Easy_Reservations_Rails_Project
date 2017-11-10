ea92850aab11c9fb1827862c9eef0052fc6889fa
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-10 16:55:40 -0600

    WIP; Created helpers to make the date and time of each reservation more user friendly

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,2 +1,9 @@
 module ReservationsHelper
+  def user_view_reservation_date(booking_datetime)
+    booking_datetime.to_date.strftime('%A, %B %d, %Y')
+  end
+
+  def user_view_reservation_time(booking_datetime)
+    booking_datetime.to_time.strftime('%l:%M %P')
+  end
 end
