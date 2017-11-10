37653847aec45a9480c42d40dd21f699b8904373
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-10 05:46:12 -0600

    WIP; Added styling and alert for users that don't have a home address and want to make a reservation

--- app/controllers/room_types_controller.rb
+++ app/controllers/room_types_controller.rb
@@ -2,9 +2,13 @@ class RoomTypesController < ApplicationController
   before_action :set_roomtype
 
   def show
+    flash.now[:alert] = 'You must have a Home Address to make a reservation' if
+      current_user && current_user.addresses.blank?
+      
     @amenities = @room_type.hotel_amenities
+    @reservation = current_user.reservations.build if current_user
   end
-  
+
   private
   def set_roomtype
     # raise params.inspect
