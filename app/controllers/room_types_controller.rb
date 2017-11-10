6315be674266de4f89fd9fe06509ba14a66fe2e7
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-10 06:22:51 -0600

    WIP; Created reservation form with virtual attributes

--- app/controllers/room_types_controller.rb
+++ app/controllers/room_types_controller.rb
@@ -4,8 +4,9 @@ class RoomTypesController < ApplicationController
   def show
     flash.now[:alert] = 'You must have a Home Address to make a reservation' if
       current_user && current_user.addresses.blank?
-      
+
     @amenities = @room_type.hotel_amenities
+
     @reservation = current_user.reservations.build if current_user
   end
 
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ActiveRecord::Base
   belongs_to :user
   belongs_to :room
+
+  attr_accessor :checkin_date, :checkin_time, :checkout_date, :checkout_time
+
 end
