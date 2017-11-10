daceb86ff40bcc31768be5a12f3bfea4be2aff9e
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-10 06:24:50 -0600

    WIP; Created strong parameters

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,6 +2,19 @@ class ReservationsController < ApplicationController
 
   def create
     raise params.inspect
-    
+
+  end
+
+  private
+  def reservation_params
+  params.require(:reservation).permit(
+    :checkin_date,
+    :checkin_time,
+    :checkout_date,
+    :checkout_time,
+    :number_of_rooms,
+    :room_id,
+    :user_id
+  )
   end
 end
