d5bd5e4ab40c39648d1d74c583379f8a92013854
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-10 07:10:14 -0600

    ​WIP; Added some custom validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -10,6 +10,48 @@ class Reservation < ActiveRecord::Base
   validates_presence_of :checkout_time
   validates_presence_of :number_of_rooms
 
+  validate :future_checkin_date
+  validate :future_checkin_time
+  validate :future_checkout_date
+  validate :future_checkout_time
+  validate :no_of_rooms_greater_then_0, if: lambda { number_of_rooms.present? }
+
+  def future_checkin_date
+    # binding.pry
+    if checkin_date.present? && checkin_date.to_date < DateTime.now.to_date
+      errors.add(:checkin_date, 'must be a valid current or future date')
+    end
+  end
+
+  def future_checkin_time
+    # binding.pry
+    if checkin_time.present? && checkin_time.to_time < Time.now
+      errors.add(:checkin_time, 'must be a valid current or future time')
+    end
+  end
+
+  def future_checkout_date
+    # binding.pry
+    if checkin_date.present? && checkout_date.present? &&
+        checkout_date.to_date < checkin_date.to_date
+      errors.add(:checkout_date, 'must be a valid date after your check in ' \
+        'date')
+    end
+  end
+
+  def future_checkout_time
+    if checkin_datetime.present? && checkout_datetime.present? &&
+        checkout_datetime <= checkin_datetime
+        errors.add(:checkout_time, 'must be a valid time after your check in ' \
+        'time.')
+    end
+  end
+
+  def no_of_rooms_greater_then_0
+    errors.add(:number_of_rooms, 'must be 1 or more to make a reservation') if
+      number_of_rooms <= 0
+  end
+  
   def convert_to_datetime
     # binding.pry
     checkin_datetime =  merge_datetime(checkin_date, checkin_time) if
