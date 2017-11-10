2827c4f29745e03a00d67471f3a49d5b41e17ad1
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-10 05:01:47 -0600

    WIP; Created class method 'from_omniauth' to find or create a user for app when they register or log in with GitHub

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,15 @@ class User < ActiveRecord::Base
       end
     end
   end
+
+  def self.from_omniauth(auth)
+  # binding.pry
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.password = SecureRandom.hex
+    end
+  end
+
 end
