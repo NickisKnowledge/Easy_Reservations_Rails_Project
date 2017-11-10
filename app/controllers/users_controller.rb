4a04f58c44a59bd913ad51a413ac12fa0536820c
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-09 20:02:34 -0600

    WIP; Added strong parameters to filter user input, also check for user errors in the view and display them, if their are any

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
 
   def create
-    raise params.inspect
-    
+    # raise params.inspect
+    @user = User.create(user_params)
+    # debugger
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+  # binding.pry
+    params.require(:user).permit(
+      :name,
+      :password,
+      :password_confirmation
+    )
   end
 end
