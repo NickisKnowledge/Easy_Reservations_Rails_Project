4e21afbd696e545914eb04707ea2ac30bc7453df
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-09 21:01:19 -0600

    WIP; Created the sessions controller, routes and new view to allow a user to log in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :addresses
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :reservations, only: [:index, :edit, :update, :destroy]
   resources :rooms, only: :show do
