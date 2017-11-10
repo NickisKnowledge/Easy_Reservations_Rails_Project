349f184022d17e2290a738e30c25b4f3287076df
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-10 05:07:26 -0600

    WIP; Made address validation conditional to allow users to register with GitHub

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,12 +22,12 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
-  binding.pry
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
       user.password = SecureRandom.hex
+      binding.pry
     end
   end
 
