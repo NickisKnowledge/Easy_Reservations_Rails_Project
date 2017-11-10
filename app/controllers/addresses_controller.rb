f164c68a92c42009fdc02dea27a4738b4afea34a
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-09 23:56:49 -0600

    WIP; Created address destroy method

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,10 @@
 class AddressesController < ApplicationController
+
+  def destroy
+    # raise params.inspect
+    address = Address.find(params[:id])
+    address.delete
+    redirect_to user_path(current_user),
+      {notice: 'Work address has been removed'}
+  end
 end
